be07e3cb972c5ea1716c5b05ee8215278efbf225
Sergey Fomenko <artem.work749@gmail.com>
2024-01-14 13:02:14 +0300

    Solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean1 == bean2);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1 == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    private String name;
+
+    public void sayMeow() {
+        System.out.println("MEOWWW!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
